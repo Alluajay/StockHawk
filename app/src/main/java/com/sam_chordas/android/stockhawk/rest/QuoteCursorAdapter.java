dcda95c1811f319994c8fef64d9b3d40c94be041
Alluajay <alludajay@gmail.com>
2016-09-04 18:09:27 +0530

    added accessibility

--- app/src/main/java/com/sam_chordas/android/stockhawk/rest/QuoteCursorAdapter.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/rest/QuoteCursorAdapter.java
@@ -50,9 +50,11 @@ public class QuoteCursorAdapter extends CursorRecyclerViewAdapter<QuoteCursorAda
   public void onBindViewHolder(final ViewHolder viewHolder, final Cursor cursor,int postion){
     String symbol =cursor.getString(cursor.getColumnIndex("symbol"));
     viewHolder.symbol.setText(cursor.getString(cursor.getColumnIndex("symbol")));
+    viewHolder.symbol.setContentDescription(cursor.getString(cursor.getColumnIndex("symbol")));
     viewHolder.itemView.setTag(symbol);
     viewHolder.itemView.setContentDescription(symbol);
     viewHolder.bidPrice.setText(cursor.getString(cursor.getColumnIndex("bid_price")));
+    viewHolder.bidPrice.setContentDescription(cursor.getString(cursor.getColumnIndex("bid_price")));
     int sdk = Build.VERSION.SDK_INT;
     if (cursor.getInt(cursor.getColumnIndex("is_up")) == 1){
       if (sdk < Build.VERSION_CODES.JELLY_BEAN){
@@ -73,8 +75,11 @@ public class QuoteCursorAdapter extends CursorRecyclerViewAdapter<QuoteCursorAda
     }
     if (Utils.showPercent){
       viewHolder.change.setText(cursor.getString(cursor.getColumnIndex("percent_change")));
+      viewHolder.change.setContentDescription(cursor.getString(cursor.getColumnIndex("percent_change")));
     } else{
       viewHolder.change.setText(cursor.getString(cursor.getColumnIndex("change")));
+      viewHolder.change.setContentDescription(cursor.getString(cursor.getColumnIndex("change")));
+
     }
   }
 
